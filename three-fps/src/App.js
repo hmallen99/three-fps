5744612c05a97a763f32a87f53364f5ac8802edf
hmallen99 <henmanallen@berkeley.edu>
2021-08-15 16:45:26 -0700

    Added fps camera

--- three-fps/src/App.js
+++ three-fps/src/App.js
@@ -1,42 +1,28 @@
 import './App.css';
-import { useRef, useState, useEffect } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { PerspectiveCamera, FlyControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Sky, PointerLockControls } from '@react-three/drei'
+import { Player } from './Player'
+import { Ground } from './Ground'
+import { Physics } from '@react-three/cannon'
 
-
-function Box(props) {
-  const mesh = useRef()
-
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="red" />
-    </mesh>
-  )
-}
 
 
 function App() {
-  // TODO: Add custom camera
-  
-
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <PerspectiveCamera makeDefault position = {[0, 0, 10]} />
-        <ambientLight intensity={0.5} />
+    <div id="canvas-container" style={{"height" : "100%"}}>
+      <Canvas
+        shadows
+        gl={{ alpha: false }}
+        camera={{ fov: 85 }}>
+        <Sky sunPosition={[100, 20, 100]} />
+        <ambientLight intensity={0.3} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-        <OrbitControls />
-        <FlyControls autoForward={false} dragToLook={false} movementSpeed={1.0} rollSpeed={1.0} />
+        <Physics gravity={[0, -30, 0]}>
+          <Ground />
+          <Player />
+        </Physics>
+        <PointerLockControls />
       </Canvas>
     </div>
   );
---
+++ three-fps/src/Gun.js
@@ -0,0 +1,16 @@
+import React, { useRef } from "react"
+
+export default function Model(props) {
+  const group = useRef()
+  return (
+    <group ref={group} dispose={null} {...props}>
+      <group rotation={[0, Math.PI / 1.8, -0.3]} scale={0.5}>
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="red" />
+        </mesh>
+      </group>
+    </group>
+  )
+}
+
