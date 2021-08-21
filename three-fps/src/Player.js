ee631924568f86fcc1aef5f7126eb687f2cc5e3c
hmallen99 <henmanallen@berkeley.edu>
2021-08-21 11:55:01 -0700

    Add double jump

--- three-fps/src/Player.js
+++ three-fps/src/Player.js
@@ -13,16 +13,83 @@ const sideVector = new THREE.Vector3()
 const rotation = new THREE.Vector3()
 const speed = new THREE.Vector3()
 
-const usePlayerControls = () => {
-  const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false })
+const usePlayerControls = (velocity) => {
+  const [movement, setMovement] = useState({
+    forward: false, 
+    backward: false, 
+    left: false,
+    right: false, 
+    jump: {
+      jump: false,
+      jumpCount: 0,
+      jumpHeld: false,
+    }
+  })
   useEffect(() => {
-    const handleKeyDown = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }))
-    const handleKeyUp = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
+    const handleKeyPress = (e) => setMovement((m) => {
+      if (moveFieldByKey(e.code) == "jump") {
+        console.log(m.jump.jumpCount)
+        if (m.jump.jumpHeld) {
+          return {
+            ...m,
+            jump: { jump: false, jumpCount: m.jump.jumpCount, jumpHeld: true }
+          }
+        }
+        else if (Math.abs(velocity.current[1]) > 0.05 && m.jump.jumpCount < 2) {
+          return {
+            ...m,
+            jump: { jump: true, jumpCount: m.jump.jumpCount + 1, jumpHeld: true }
+          }
+        }
+        else if (Math.abs(velocity.current[1]) > 0.05 && m.jump.jumpCount >= 2) {
+          return {
+            ...m,
+            jump: { jump: false, jumpCount: m.jump.jumpCount + 1, jumpHeld: true}
+          }
+        }
+        else {
+          return {
+            ...m,
+            jump: { jump: true, jumpCount: 1, jumpHeld: true }
+          }
+        }
+      }
+      else {
+        return {
+          ...m,
+        }
+      }
+    })
+
+    const handleKeyDown = (e) => setMovement((m) => {
+      
+      if (moveFieldByKey(e.code) != "jump") {
+        return { ...m, [moveFieldByKey(e.code)]: true }
+      }
+      else {
+        return { ...m }
+      }
+    })
+
+    const handleKeyUp = (e) => setMovement((m) => {
+      if (moveFieldByKey(e.code) != "jump") {
+        return { ...m, [moveFieldByKey(e.code)]: false }
+      }
+      else {
+        return {
+          ...m,
+          jump: { jump: false, jumpCount: m.jump.jumpCount, jumpHeld: false }
+        }
+      }
+    })
+
     document.addEventListener("keydown", handleKeyDown)
     document.addEventListener("keyup", handleKeyUp)
+    document.addEventListener("keypress", handleKeyPress)
     return () => {
       document.removeEventListener("keydown", handleKeyDown)
       document.removeEventListener("keyup", handleKeyUp)
+      document.removeEventListener("keypress", handleKeyPress)
     }
   }, [])
   return movement
@@ -31,10 +98,10 @@ const usePlayerControls = () => {
 export const Player = (props) => {
   const gun = useRef()
   const [ref, api] = useSphere(() => ({ mass: 1, type: "Dynamic", position: [0, 10, 0], ...props }))
-  const { forward, backward, left, right, jump } = usePlayerControls()
   const { camera } = useThree()
   const velocity = useRef([0, 0, 0])
   useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), [])
+  const { forward, backward, left, right, jump } = usePlayerControls(velocity)
   useFrame((state) => {
     ref.current.getWorldPosition(camera.position)
     frontVector.set(0, 0, Number(backward) - Number(forward))
@@ -49,7 +116,7 @@ export const Player = (props) => {
     gun.current.rotation.copy(camera.rotation)
     gun.current.position.copy(camera.position).add(camera.getWorldDirection(rotation).multiplyScalar(1))
     api.velocity.set(direction.x, velocity.current[1], direction.z)
-    if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) api.velocity.set(velocity.current[0], 10, velocity.current[2])
+    if (jump.jump) api.velocity.set(velocity.current[0], 10, velocity.current[2])
   })
   return (
     <>
